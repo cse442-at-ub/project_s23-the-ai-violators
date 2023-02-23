e1ed2bff5684fea70a7381857cafd7e88265ee3e
Sebastian Richel <sebastianrichel@gmail.com>
2023-02-23 04:32:32 +0000

    #20 can get collections from mongo in dev container

--- database.php
+++ database.php
@@ -1,17 +1,13 @@
 <?php
-// PHP mongo install guide: https://www.php.net/manual/en/mongodb.installation.php
-
-// phpinfo();
     require_once __DIR__ . '/vendor/autoload.php';
-    $client = new MongoDB\Client('mongodb://0.0.0.0:27017');
-    echo("Connected to MongoDB successfully!");
-    while (true) {
-        // $collection = $client->test->users;
-        // $result = $collection->insertOne(['x' => 1]);
-        // echo "Inserted with Object ID '{$result->getInsertedId()}'";
-        sleep(1);
+    $client = new MongoDB\Client('mongodb://mongo:27017');
+
+    echo("Connected to MongoDB successfully!\n");
+
+    $tables = $client->listDatabases();
+    foreach ($tables as $table) {
+        echo $table['name'];
+        echo "\n";
     }
-
-    // some extra stuff more stuff
-
+        
 ?>
