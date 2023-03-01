cace622a555736f95624d813083c550407513dc2
Sebastian Richel <sebastianrichel@gmail.com>
2023-03-01 23:56:01 +0000

    now uses mysql

--- database.php
+++ database.php
@@ -1,13 +1,13 @@
 <?php
-    require_once __DIR__ . '/vendor/autoload.php';
-    $client = new MongoDB\Client('mongodb://mongoDB:27017');
+   // connect to the mySQL database and print all table names
+   $mysqli = mysqli_connect("db", "admin", "password", "test_db");
+   $result = mysqli_query($mysqli, "SHOW TABLES");
+   while ($row = mysqli_fetch_row($result)) {
+      echo "Table: {$row[0]}\n";
+   }
 
-    echo("Connected to MongoDB successfully!\n");
 
-    $tables = $client->listDatabases();
-    foreach ($tables as $table) {
-        echo $table['name'];
-        echo "\n";
-    }
-        
+
+   mysqli_free_result($result);
+   mysqli_close($mysqli);     
 ?>
