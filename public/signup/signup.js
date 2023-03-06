951ff642ce74391a8e2b7fde820ef0046d60bfd3
Dohello <jakeguzzardo2010@gmail.com>
2023-03-05 22:52:06 -0500

    idk if this even works

---
+++ public/signup/signup.js
@@ -0,0 +1,20 @@
+let password = document.getElementById("password")
+let confirm = document.getElementById("confirmPassword")
+let submit = document.getElementById("submitButton")
+
+let form = document.getElementsByTagName("form")[0];
+
+function check(){
+    if(password.value != confirm.value){
+        confirm.setCustomValidity("Passwords must be matching")
+    } else {
+        confirm.setCustomValidity('')
+    }
+}
+
+form.addEventListener("submit", e => {
+   e.preventDefault()
+   let success = document.querySelector(".success-msg")
+   success.style.display = "block"
+})
+
