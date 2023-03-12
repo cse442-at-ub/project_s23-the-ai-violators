56eadeb4bb35d8a19585d2c45754d6de21132f52
Cameron <cjheffle@buffalo.edu>
2023-03-12 01:53:52 +0000

    adding tracking page with html, css, and js

---
+++ public/track/track.js
@@ -0,0 +1,21 @@
+let breakfast = document.getElementById("breakfast");
+breakfast.addEventListener('click', function() {
+    breakfast.classList.add('active');
+});
+
+let lunch = document.getElementById('lunch');
+lunch.addEventListener('click', function() {
+    lunch.classList.add('active');
+});
+
+let dinner = document.getElementById('dinner');
+dinner.addEventListener('click', function() {
+    dinner.classList.add('active');
+});
+
+let snack = document.getElementById('snack');
+snack.addEventListener('click', function() {
+  snack.classList.add('active');
+});
+
+
