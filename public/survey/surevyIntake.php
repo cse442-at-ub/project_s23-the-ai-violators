78fcc0821d2e19b38247e2553fee946aeda1c235
adrienguan2 <adriensh@buffalo.edu>
2023-03-14 23:18:44 +0000

    #35 added a wrapper file

---
+++ public/survey/surevyIntake.php
@@ -0,0 +1,13 @@
+<?php
+
+    require __DIR__ . '/../../config/database.php';
+
+
+    $date = $_POST['date'];
+    $calories = $_POST['calories'];
+    $protein = $_POST['protein'];
+    $carbs = $_POST['carbs'];
+    $fats = $_POST['fats'];
+
+    trackCaloriesAndMacros(69, $date, $calories, $protein, $carbs, $fats);
+
