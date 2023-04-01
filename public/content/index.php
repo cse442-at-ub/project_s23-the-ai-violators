092120bc128735ba0f53ccf066693093da2520d8
Sebastian Richel <sebastianrichel@gmail.com>
2023-04-01 23:37:00 +0000

    #51 session login works

--- public/content/index.php
+++ public/content/index.php
@@ -136,4 +136,5 @@ if (!isset($_SESSION['user_name'])) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </body>
 
+
 </html>
