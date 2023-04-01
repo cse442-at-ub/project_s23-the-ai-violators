d3422ec00a9424641fbb3a1caed402f8f617b455
Sebastian Richel <sebastianrichel@gmail.com>
2023-04-01 23:43:55 +0000

    #53 HOTFIX navbar links

--- public/content/index.php
+++ public/content/index.php
@@ -32,10 +32,10 @@ if (!isset($_SESSION['user_name'])) {
         <div><img id="carrot" src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/image/carrot.png" alt="">
             <p id="logoName">nutr.io</p>
         </div>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/index.html">Profile
+        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/">Profile
             Page</a>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/track/index.html">Track Page</a>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login/index.html">Logout</a>
+        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/track/">Track Page</a>
+        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login/">Logout</a>
         <div>
 
             <div id="username">%Cusername%</div>
