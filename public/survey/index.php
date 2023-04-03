5bc5dd07ea183a450cb04ed3b475141343bec0ff
adrienguan2 <adriensh@buffalo.edu>
2023-04-03 16:32:47 +0000

    #5 fixed get started button to redirect to content

--- public/survey/index.php
+++ public/survey/index.php
@@ -139,9 +139,11 @@ if (!isset($_SESSION['user_name'])) {
 
         <div>
         
-          <button id="submitButton" type="submit">Get Started</button>
+          <button id="submitButton" type="submit"  onclick="window.location.href= 'www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/content/'">
+          Get Started
+          </button>
         
-      </div>
+      </div> 
      
       </form>
       
