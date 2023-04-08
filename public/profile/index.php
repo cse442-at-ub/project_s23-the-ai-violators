aea54774747b17a1114c13194b6d44d8593a9322
adrienguan2 <adriensh@buffalo.edu>
2023-04-08 23:40:16 +0000

    #63 converted the profile edit page to bootstrap, needs font fixed and save profile button

--- public/profile/index.php
+++ public/profile/index.php
@@ -63,11 +63,11 @@ $userInfo = getUserInfo($_SESSION['user_name']);
     <p>
 
 
-    <div class="col-md-4 mb-3">
+    <div class=".col6">
         <div>
             <div class="card-body">
                 <div class="d-flex flex-column align-items-center text-center">
-                    <p><?php echo $_SESSION['user_name'] ?></p>
+                    <p class="fs-3"><?php echo $_SESSION['user_name'] ?></p>
                     <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/user.png" alt="Admin" class="rounded-circle" width="150">
                     <div class="mt-3">
                         <p class="text-secondary mb-1">timmy2time@gmail.com</p>
@@ -78,68 +78,68 @@ $userInfo = getUserInfo($_SESSION['user_name']);
         </div>
     </div>
 
-    <div class="col-md-8">
+    <div class=".col6">
         <div class="card mb-3">
-            <div class="card-body">
+            <div class="card-body align-items-center text-center">
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Sex:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[4] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Height:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[1] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Weight:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[2] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Current Goal:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[10] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Focus Macro:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[11] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Current Calorie Goal:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
                         <?php echo $userInfo[6] ?>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-6">
                         <h6 class="mb-0">Restrictions:</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-6 text-secondary">
 
                     </div>
                 </div>
--- public/profileEdit/index.php
+++ public/profileEdit/index.php
@@ -7,172 +7,187 @@ if (!isset($_SESSION['user_name'])) {
     exit();
 }
 
-?> 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profileEdit/profileEdit.css">
-  <link rel="stylesheet" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profileEdit/profileEdit.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="profileEdit_test.css">
 </head>
 
 
-    <body>
-    <form method="POST" action = "editIntake.php">
-        
-        <div class = "userIcon">
-            <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/user.png" alt="">
-        </div>
-        
+<body>
+    <form method="POST" action="editIntake.php">
 
 
-        <h1>Your Profile</h1>
-        <h2>Here's a look at you...</h2>
-       
-        <div id ="profileForm">
-        </div>
+        <nav class="navbar navbar-expand-lg bg-secondary-subtle">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/image/carrot.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    nutr.io
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/content/">Content Page</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/track/">Track Page</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login/" onclick="sessionStorage.removeItem('username')">Logout</a>
+                        </li>
 
-        <div id ="userName">
-            <p> <?php echo $_SESSION['user_name'] ?> </p>
-        </div>
+                    </ul>
 
-        
-        <div id = "logout">
-            <button id="logoutButton" type="button" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login'; sessionStorage.removeItem('username')">Logout</button>
-        </div>
-
-        <div id = "editProfile">
-            <button id="submitButton" type="submit">
-            Save
-            </button>
-        </div>
-
-
-        <div id ="email">
-            <p>chad69@gmail.com</p>
-        </div>
-        
-    
-        
-        <div class="navbar">
-        <a id="NUTRIO" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/content">
-            <div>
-                <img id="carrot" src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/image/carrot.png" alt="">
-                <p id="logoName">nutr.io</p>
+                </div>
             </div>
-        </a>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/">Profile
-            Page</a>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/track/">Track Page</a>
-        <a href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login/">Logout</a>
-        <div>
+        </nav>
 
 
-        
-        <div id = "sex">
-            <p>Sex:</p>
+
+
+
+        <p class="fs-1">Your Profile
+        <p>
+        <p class="fs-4">Here's a look at you...
+        <p>
+
+
+        <div class=".col6">
+            <div>
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <p class="fs-3"><?php echo $_SESSION['user_name'] ?></p>
+                        <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/user.png" alt="Admin" class="rounded-circle" width="150">
+                        <div class="mt-3">
+                            <p class="text-secondary mb-1">timmy2time@gmail.com</p>
+                            <button class="btn btn-primary" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login'; sessionStorage.removeItem('username')">Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div id = "sexEdit">
-            <select name = "sex">
-              <option value="" disabled selected>Select your sex...</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other/Prefer Not To Say</option>
-            </select>
-        </div>
-        
-        <div id = "height">
-            <p>Height:</p>
-        </div>
-
-        <div id = "heightEdit">
-            <input type="number" name="height" placeholder="Enter Your Height(In)" min = "1" max="999" oninput="check()" >
-        </div>
-
-        <div id = "weight">
-            <p>Weight:</p>
-        </div>
-
-        <div id = "weightEdit">
-            <input type="number" name="weight" placeholder="Enter Your Weight(lbs)" min = "1" max="999" oninput="check()" >
-        </div>
-
-        <div id = "curgoal">
-            <p>Current Goal:</p>
-        </div>
-
-        <div id = "curgoalEdit">
-            <select name="goal" id="goal">
-              <option value="" disabled selected>Select your goal...</option>
-              <option value="cut">Lose Weight/Cut</option>
-              <option value="bulk">Gain Weight/Bulk</option>
-              <option value="maintain">Maintain</option>
-            </select>  
-        </div>
-
-        <div id = "macro">
-            <p>Focus Macro:</p>
-        </div>
-
-        <div id = "macroEdit">
-            <select name="macros" id="goal">
-              <option value="" disabled selected>Select your macro...</option>
-              <option value="protien">Proteins</option>
-              <option value="carb">Carbohydrates</option>
-              <option value="fat">Fats</option>
-            </select>  
-        </div>
-
-        <div id = "calgoal">
-            <p>Current Calorie Goal:</p>
-        </div>
-
-        <div id = "calgoalEdit">
-            <input type="number" name="targetCAL" id="calorie" placeholder="Enter Your Goal" min = "1" max="9999999" oninput="check()">
-        </div>
-
-        
-        <div id = "restrict">
-            <p>Restrictions:</p>
-        </div>
-
-        <div id = "restrictEdit">
-        <input type="checkbox" id="lactose" name="lactose" checked>
-          <label for="lactose">Lactose Intolerance</label>
-          <input type="checkbox" id="" name="gluten" checked>
-          <label for="gluten">Gluten Intolerance</label>
-          <input type="checkbox" id="vegetarian" name="vegetarian" checked>
-          <label for="vegetarian">Vegetarian</label>
-          <input type="checkbox" id="vegan" name="vegan" checked>
-          <label for="vegan">Vegan</label>
-          <input type="checkbox" id="kosher" name="kosher" checked>
-          <label for="kosher">Kosher</label>
-          <input type="checkbox" id="dairy" name="dairy" checked>
-          <label for="dairy">Dairy Free</label>
-          <input type="checkbox" id="peanuts" name="peanuts" checked>
-          <label for="peanuts">Peanut Allergy</label>
-          <input type="checkbox" id="fish" name="fish" checked>
-          <label for="fish">Fish/Shellfish Allergy</label>
-          <input type="checkbox" id="wheat" name="wheat" checked>
-          <label for="wheat">Wheat Allergy</label>
-        </div>
-
-
-
-
+        <div class=".col6">
+            <div class="card mb-3">
+                <div class="card-body align-items-center text-center">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Sex:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <select name="sex">
+                                <option value="" disabled selected>Select your sex...</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other/Prefer Not To Say</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Height:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <input type="number" name="height" placeholder="Enter Your Height(In)" min="1" max="999" oninput="check()">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Weight:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <input type="number" name="weight" placeholder="Enter Your Weight(lbs)" min="1" max="999" oninput="check()">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Current Goal:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <select name="goal" id="goal">
+                                <option value="" disabled selected>Select your goal...</option>
+                                <option value="cut">Lose Weight/Cut</option>
+                                <option value="bulk">Gain Weight/Bulk</option>
+                                <option value="maintain">Maintain</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Focus Macro:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <select name="macros" id="goal">
+                                <option value="" disabled selected>Select your macro...</option>
+                                <option value="protien">Proteins</option>
+                                <option value="carb">Carbohydrates</option>
+                                <option value="fat">Fats</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Current Calorie Goal:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <input type="number" name="targetCAL" id="calorie" placeholder="Enter Your Goal" min="1" max="9999999" oninput="check()">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">Restrictions:</h6>
+                        </div>
+                        <div class="col-sm-6 text-secondary">
+                            <input type="checkbox" id="lactose" name="lactose" checked>
+                            <label for="lactose">Lactose Intolerance</label>
+                            <input type="checkbox" id="" name="gluten" checked>
+                            <label for="gluten">Gluten Intolerance</label>
+                            <input type="checkbox" id="vegetarian" name="vegetarian" checked>
+                            <label for="vegetarian">Vegetarian</label>
+                            <input type="checkbox" id="vegan" name="vegan" checked>
+                            <label for="vegan">Vegan</label>
+                            <input type="checkbox" id="kosher" name="kosher" checked>
+                            <label for="kosher">Kosher</label>
+                            <input type="checkbox" id="dairy" name="dairy" checked>
+                            <label for="dairy">Dairy Free</label>
+                            <input type="checkbox" id="peanuts" name="peanuts" checked>
+                            <label for="peanuts">Peanut Allergy</label>
+                            <input type="checkbox" id="fish" name="fish" checked>
+                            <label for="fish">Fish/Shellfish Allergy</label>
+                            <input type="checkbox" id="wheat" name="wheat" checked>
+                            <label for="wheat">Wheat Allergy</label>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <button class="btn btn-primary" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile'">Save Changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
     </form>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</body>
 
 </html>
