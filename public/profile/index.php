201be7ca04dd7c4b204bc31845414a8fc3e1bb90
adrienguan2 <adriensh@buffalo.edu>
2023-04-08 18:11:30 +0000

    #50 committing to switch to proper editing branch without losing changes

--- public/profile/index.php
+++ public/profile/index.php
@@ -62,96 +62,173 @@ $userInfo = getUserInfo($_SESSION['user_name']);
     <p class="fs-4">Here's a look at you...
     <p>
 
-    <div class="row gutters-sm">
-        <div class="col-md-4 mb-3">
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex flex-column align-items-center text-center">
-                        <p><?php echo $_SESSION['user_name'] ?></p>
-                        <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/user.png" alt="Admin" class="rounded-circle" width="150">
-                        <div class="mt-3">
-                            <p class="text-secondary mb-1">timmy2time@gmail.com</p>
-                            <button class="btn btn-primary" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login'; sessionStorage.removeItem('username')">Logout</button>
-                        </div>
+
+    <div class="col-md-4 mb-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <p><?php echo $_SESSION['user_name'] ?></p>
+                    <img src="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/user.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                        <p class="text-secondary mb-1">timmy2time@gmail.com</p>
+                        <button class="btn btn-primary" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/login'; sessionStorage.removeItem('username')">Logout</button>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-
-
-
-
-
-
-
-
-
-        
-
-            <div id="editProfile">
-                <button id="submitButton" type="button" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profileEdit/'">
-                    Edit Profile
-                </button>
+    <div class="col-md-8">
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Sex:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[4] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Height:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[1] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Weight:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[2] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Current Goal:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[10] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Focus Macro:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[11] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Current Calorie Goal:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <?php echo $userInfo[6] ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Restrictions:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a class="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
+                    </div>
+                </div>
             </div>
-        
-            <div id="sex">
-                <p>Sex:</p>
-            </div>
-
-            <div id="sexDisplay">
-                <p> <?php echo $userInfo[4] ?> </p>
-            </div>
-
-
-            <div id="height">
-                <p>Height:</p>
-            </div>
-
-            <div id="heightDisplay">
-                <p> <?php echo $userInfo[1] ?> </p>
-            </div>
-
-            <div id="weight">
-                <p>Weight:</p>
-            </div>
-
-            <div id="weightDisplay">
-                <p> <?php echo $userInfo[2] ?> </p>
-            </div>
-
-
-            <div id="curgoal">
-                <p>Current Goal:</p>
-            </div>
-
-            <div id="goalDisplay">
-                <p> <?php echo $userInfo[10] ?> </p>
-            </div>
-
-            <div id="macro">
-                <p>Focus Macro:</p>
-            </div>
-
-            <div id="macroDisplay">
-                <p> <?php echo $userInfo[11] ?> </p>
-            </div>
-
-            <div id="calgoal">
-                <p>Current Calorie Goal:</p>
-            </div>
-
-            <div id="calDisplay">
-                <p> <?php echo $userInfo[6] ?> </p>
-            </div>
-
-            <div id="restrict">
-                <p>Restrictions:</p>
-            </div>
+        </div>
 
 
 
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+
+
+
+
+
+
+
+        <div id="editProfile">
+            <button id="submitButton" type="button" onClick="window.location.href='/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profileEdit/'">
+                Edit Profile
+            </button>
+        </div>
+
+        <div id="sex">
+            <p>Sex:</p>
+        </div>
+
+        <div id="sexDisplay">
+            <p> <?php echo $userInfo[4] ?> </p>
+        </div>
+
+
+        <div id="height">
+            <p>Height:</p>
+        </div>
+
+        <div id="heightDisplay">
+            <p> <?php echo $userInfo[1] ?> </p>
+        </div>
+
+        <div id="weight">
+            <p>Weight:</p>
+        </div>
+
+        <div id="weightDisplay">
+            <p> <?php echo $userInfo[2] ?> </p>
+        </div>
+
+
+        <div id="curgoal">
+            <p>Current Goal:</p>
+        </div>
+
+        <div id="goalDisplay">
+            <p> <?php echo $userInfo[10] ?> </p>
+        </div>
+
+        <div id="macro">
+            <p>Focus Macro:</p>
+        </div>
+
+        <div id="macroDisplay">
+            <p> <?php echo $userInfo[11] ?> </p>
+        </div>
+
+        <div id="calgoal">
+            <p>Current Calorie Goal:</p>
+        </div>
+
+        <div id="calDisplay">
+            <p> <?php echo $userInfo[6] ?> </p>
+        </div>
+
+        <div id="restrict">
+            <p>Restrictions:</p>
+        </div>
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
 
 </body>
