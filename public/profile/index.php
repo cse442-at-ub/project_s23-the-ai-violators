301e68bbe629b8b71b5233db16a82b75d7cc09d2
adrienguan2 <adriensh@buffalo.edu>
2023-04-15 18:04:56 +0000

    #50 added css address

--- public/profile/index.php
+++ public/profile/index.php
@@ -21,8 +21,9 @@ $userRestrictions = getRestrictions($_SESSION['user_name']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="profile_test.css">
+    <link rel="stylesheet" href="/CSE442-542/2023-Spring/cse-442g/project_s23-the-ai-violators/public/profile/profile_test.css">
 
 </head>
 
